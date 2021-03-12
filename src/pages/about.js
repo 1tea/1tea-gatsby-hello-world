aa371a9bc698128fa75c2dc4dd780f5b99d8ffbc
1tea <elliot@smartcasual.work>
2021-03-12 22:01:17 +0000

    add basic styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import Header from "../components/header"
 
 export default function About(){
     return(
-        <div style={{color:'teal'}}>
+        <div>
             <Header text="this is a prop header"/>
             <h1>This is the About Page</h1>
             <p>All about us, baby</p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,12 @@ import Header from "../components/header"
 
 export default function Home() {
   return (
-  <div style={{color:'purple'}}>
-    <Link to="/contact/">Contact</Link>
+  <div>
+    <Link to="/contact/">Contact</Link><br></br>
+    <Link to="/about-css-modules/">About CSS Modules</Link>
     <Header text="Hello Gatsby"/>
-    <p>What a world</p>
+    <p>CSS Practice</p>
+    <p>testy testy</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>
   );
